b04a9afe8c0421c233eb6144f001937c697b2ce9
Elizabeth Kim <elizabeth.kim93@gmail.com>
2019-12-08 18:33:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,10 +9,10 @@ def load_library(file_path)
   }
 
   emotions.each do |key,value|
-    new_hash['get_emoticon'][value[0]] + emoticons[key][1]
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
     new_hash['get_meaning'][value[1]] = key
   end
-  new_hash  
+  new_hash
   # code goes here
 end
 
